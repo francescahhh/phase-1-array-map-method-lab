93b6b084f331401f631ea2cf19f4a9d6bc8bd417
Francesca Hamm <francesca.c.hamm@gmail.com>
2021-11-13 10:12:42 -0700

    commit tree deee3c8430113e7885d400b7ff6211ffd48500d6

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
-const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
-];
 
-const titleCased = () => {
-  return tutorials
+//tutorials.map(titleCased)
+
+function titleCased() {
+  const tutorials = [
+    'what does the this keyword mean?',
+    'What is the Constructor OO pattern?',
+    'implementing Blockchain Web API',
+    'The Test Driven Development Workflow',
+    'What is NaN and how Can we Check for it',
+    'What is the difference between stopPropagation and preventDefault?',
+    'Immutable State and Pure Functions',
+    'what is the difference between == and ===?',
+    'what is the difference between event capturing and bubbling?',
+    'what is JSONP?'
+  ];
+
+  return tutorials.map(function(string) {
+    return string.split(' ').map(function(string) {
+      return string.replace(string[0], string[0].toUpperCase());
+    }).join(' ');
+  })
 }
+// return tutorials.split(' ').map(function(string) {
+  //    return string.replace(string[0], string[0].toUpperCase());
+  //  }).join(' ');
